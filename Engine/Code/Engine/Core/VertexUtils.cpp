b3b7a55798fa523df04b22ebe5273721cb2f2a4a
(C35) Ruoxin Sun <sylviasun@smu.edu>
2025-10-27 23:27:28 -0600

    Add vertex utility functions
    [git-p4: depot-paths = "//depot/C35/Students/sylviasun/SD/": change = 435758]

--- Engine/Code/Engine/Core/VertexUtils.cpp
+++ Engine/Code/Engine/Core/VertexUtils.cpp
@@ -1,6 +1,8 @@
 #include "Engine/Core/VertexUtils.hpp"
 #include "Engine/Core/Vertex.hpp"
+#include "Engine/Math/AABB2.hpp"
 #include "Engine/Math/MathUtils.hpp"
+#include "Engine/Math/OBB2.hpp"
 #include "Engine/Math/Vec2.hpp"
 
 #include <math.h>
@@ -15,4 +17,142 @@ void TransformVertexArrayXY3D( int numVerts, Vertex* verts, float uniformScaleXY
 	{
 		TransformPositionXY3D( verts[vertexIndex].m_position, iBasis, jBasis, translationXY );
 	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForDisc2D( std::vector<Vertex>& verts, Vec2 const& discCenter, float discRadius, Rgba8 const& color, int numSides )
+{
+	float deltaDegrees = 360.f / static_cast<float>( numSides );
+	for ( int sideIndex = 0; sideIndex < numSides; ++sideIndex )
+	{
+		float degreesA = deltaDegrees * static_cast<float>( sideIndex );
+		float degreesB = deltaDegrees * static_cast<float>( ( sideIndex + 1 ) % numSides );
+		Vec2 pointA = discCenter + Vec2::MakeFromPolarDegrees( degreesA, discRadius );
+		Vec2 pointB = discCenter + Vec2::MakeFromPolarDegrees( degreesB, discRadius );
+		verts.push_back( Vertex( Vec3( discCenter.x, discCenter.y, 0.f ), color, Vec2( 0.5f, 0.5f ) ) );
+		verts.push_back( Vertex( Vec3( pointA.x, pointA.y, 0.f ), color, Vec2( 0.5f + 0.5f * CosDegrees( degreesA ), 0.5f + 0.5f * SinDegrees( degreesA ) ) ) );
+		verts.push_back( Vertex( Vec3( pointB.x, pointB.y, 0.f ), color, Vec2( 0.5f + 0.5f * CosDegrees( degreesB ), 0.5f + 0.5f * SinDegrees( degreesB ) ) ) );
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForRing2D( std::vector<Vertex>& verts, Vec2 const& ringCenter, float ringRadius, float thickness, Rgba8 const& color, int numSides )
+{
+	float deltaDegrees = 360.f / static_cast<float>( numSides );
+	float innerRadius = ringRadius - ( thickness * 0.5f );
+	float outerRadius = ringRadius + ( thickness * 0.5f );
+	for ( int sideIndex = 0; sideIndex < numSides; ++sideIndex )
+	{
+		float degreesA = deltaDegrees * static_cast<float>( sideIndex );
+		float degreesB = deltaDegrees * static_cast<float>( ( sideIndex + 1 ) % numSides );
+		Vec2 innerPointA = ringCenter + Vec2::MakeFromPolarDegrees( degreesA, innerRadius );
+		Vec2 innerPointB = ringCenter + Vec2::MakeFromPolarDegrees( degreesB, innerRadius );
+		Vec2 outerPointA = ringCenter + Vec2::MakeFromPolarDegrees( degreesA, outerRadius );
+		Vec2 outerPointB = ringCenter + Vec2::MakeFromPolarDegrees( degreesB, outerRadius );
+		verts.push_back( Vertex( Vec3( innerPointA.x, innerPointA.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+		verts.push_back( Vertex( Vec3( outerPointA.x, outerPointA.y, 0.f ), color, Vec2( 1.f, 0.f ) ) );
+		verts.push_back( Vertex( Vec3( outerPointB.x, outerPointB.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+		verts.push_back( Vertex( Vec3( innerPointA.x, innerPointA.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+		verts.push_back( Vertex( Vec3( outerPointB.x, outerPointB.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+		verts.push_back( Vertex( Vec3( innerPointB.x, innerPointB.y, 0.f ), color, Vec2( 0.f, 1.f ) ) );
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForAABB2D( std::vector<Vertex>& verts, AABB2 const& alignedBox, Rgba8 const& color )
+{
+	verts.push_back( Vertex( Vec3( alignedBox.m_mins.x, alignedBox.m_mins.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( alignedBox.m_maxs.x, alignedBox.m_mins.y, 0.f ), color, Vec2( 1.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( alignedBox.m_maxs.x, alignedBox.m_maxs.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( alignedBox.m_mins.x, alignedBox.m_mins.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( alignedBox.m_maxs.x, alignedBox.m_maxs.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( alignedBox.m_mins.x, alignedBox.m_maxs.y, 0.f ), color, Vec2( 0.f, 1.f ) ) );
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForOBB2D( std::vector<Vertex>& verts, OBB2 const& orientedBox, Rgba8 const& color )
+{
+	Vec2 corners[4];
+	orientedBox.GetCornerPoints( corners );
+	verts.push_back( Vertex( Vec3( corners[0].x, corners[0].y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( corners[1].x, corners[1].y, 0.f ), color, Vec2( 1.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( corners[2].x, corners[2].y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( corners[0].x, corners[0].y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( corners[2].x, corners[2].y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( corners[3].x, corners[3].y, 0.f ), color, Vec2( 0.f, 1.f ) ) );
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForCapsule2D( std::vector<Vertex>& verts, Vec2 const& boneStart, Vec2 const& boneEnd, float radius, Rgba8 const& color, int numSides )
+{
+	Vec2 boneVector = boneEnd - boneStart;
+	Vec2 boneDirection = boneVector.GetNormalized();
+	Vec2 perpendicular = boneDirection.GetRotatedBy90Degrees();
+	// Add cylinder part
+	Vec2 topLeft = boneStart + perpendicular * radius;
+	Vec2 topRight = boneStart - perpendicular * radius;
+	Vec2 bottomLeft = boneEnd + perpendicular * radius;
+	Vec2 bottomRight = boneEnd - perpendicular * radius;
+	verts.push_back( Vertex( Vec3( topLeft.x, topLeft.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( bottomLeft.x, bottomLeft.y, 0.f ), color, Vec2( 0.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( bottomRight.x, bottomRight.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( topLeft.x, topLeft.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( bottomRight.x, bottomRight.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( topRight.x, topRight.y, 0.f ), color, Vec2( 1.f, 0.f ) ) );
+	// Add end caps
+	float deltaDegrees = 180.f / static_cast<float>( numSides );
+	Vec2 centerTop = boneStart;
+	Vec2 centerBottom = boneEnd;
+	for ( int sideIndex = 0; sideIndex < numSides; ++sideIndex )
+	{
+		float degreesA = deltaDegrees * static_cast<float>( sideIndex );
+		float degreesB = deltaDegrees * static_cast<float>( sideIndex + 1 );
+		// Top cap
+		Vec2 pointA_Top = centerTop + ( perpendicular.GetRotatedByDegrees( degreesA ) * radius );
+		Vec2 pointB_Top = centerTop + ( perpendicular.GetRotatedByDegrees( degreesB ) * radius );
+		verts.push_back( Vertex( Vec3( centerTop.x, centerTop.y, 0.f ), color, Vec2( 0.5f, 0.5f ) ) );
+		verts.push_back( Vertex( Vec3( pointA_Top.x, pointA_Top.y, 0.f ), color, Vec2( 0.5f + 0.5f * CosDegrees( degreesA ), 0.5f + 0.5f * SinDegrees( degreesA ) ) ) );
+		verts.push_back( Vertex( Vec3( pointB_Top.x, pointB_Top.y, 0.f ), color, Vec2( 0.5f + 0.5f * CosDegrees( degreesB ), 0.5f + 0.5f * SinDegrees( degreesB ) ) ) );
+		// Bottom cap
+		Vec2 pointA_Bottom = centerBottom + ( ( -perpendicular ).GetRotatedByDegrees( degreesA ) * radius );
+		Vec2 pointB_Bottom = centerBottom + ( ( -perpendicular ).GetRotatedByDegrees( degreesB ) * radius );
+		verts.push_back( Vertex( Vec3( centerBottom.x, centerBottom.y, 0.f ), color, Vec2( 0.5f, 0.5f ) ) );
+		verts.push_back( Vertex( Vec3( pointB_Bottom.x, pointB_Bottom.y, 0.f ), color, Vec2( 0.5f + 0.5f * CosDegrees( degreesB ), 0.5f + 0.5f * SinDegrees( -degreesB ) ) ) );
+		verts.push_back( Vertex( Vec3( pointA_Bottom.x, pointA_Bottom.y, 0.f ), color, Vec2( 0.5f + 0.5f * CosDegrees( degreesA ), 0.5f + 0.5f * SinDegrees( -degreesA ) ) ) );
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForTriangle2D( std::vector<Vertex>& verts, Vec2 const& ccw0, Vec2 const& ccw1, Vec2 const& ccw2, Rgba8 const& color )
+{
+	// Counter-clockwise
+	verts.push_back( Vertex( Vec3( ccw0.x, ccw0.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( ccw1.x, ccw1.y, 0.f ), color, Vec2( 1.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( ccw2.x, ccw2.y, 0.f ), color, Vec2( 0.5f, 1.f ) ) );
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForLineSegment2D( std::vector<Vertex>& verts, Vec2 const& start, Vec2 const& end, float thickness, Rgba8 const& color )
+{
+	Vec2 lineVector = end - start;
+	Vec2 lineDirection = lineVector.GetNormalized();
+	Vec2 perpendicular = lineDirection.GetRotatedBy90Degrees();
+	Vec2 halfThicknessOffset = perpendicular * ( thickness * 0.5f );
+	Vec2 topLeft = start + halfThicknessOffset;
+	Vec2 topRight = start - halfThicknessOffset;
+	Vec2 bottomLeft = end + halfThicknessOffset;
+	Vec2 bottomRight = end - halfThicknessOffset;
+	verts.push_back( Vertex( Vec3( topLeft.x, topLeft.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( bottomLeft.x, bottomLeft.y, 0.f ), color, Vec2( 0.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( bottomRight.x, bottomRight.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( topLeft.x, topLeft.y, 0.f ), color, Vec2( 0.f, 0.f ) ) );
+	verts.push_back( Vertex( Vec3( bottomRight.x, bottomRight.y, 0.f ), color, Vec2( 1.f, 1.f ) ) );
+	verts.push_back( Vertex( Vec3( topRight.x, topRight.y, 0.f ), color, Vec2( 1.f, 0.f ) ) );
 }
--- Engine/Code/Engine/Core/VertexUtils.hpp
+++ Engine/Code/Engine/Core/VertexUtils.hpp
@@ -1,10 +1,22 @@
 #pragma once
+#include "Engine/Core/Vertex.hpp"
+#include <vector>
 
 
 //-----------------------------------------------------------------------------------------------
-struct Vec2;   // Forward declaration
-struct Vertex; // Forward declaration
+struct AABB2;
+struct OBB2;
 
 
 //-----------------------------------------------------------------------------------------------
 void TransformVertexArrayXY3D( int numVerts, Vertex* verts, float uniformScaleXY, float rotationDegreesAboutZ, Vec2 const& translationXY );
+
+
+//-----------------------------------------------------------------------------------------------
+void AddVertsForDisc2D( std::vector<Vertex>& verts, Vec2 const& discCenter, float discRadius, Rgba8 const& color, int numSides );
+void AddVertsForRing2D( std::vector<Vertex>& verts, Vec2 const& ringCenter, float ringRadius, float thickness, Rgba8 const& color, int numSides );
+void AddVertsForAABB2D( std::vector<Vertex>& verts, AABB2 const& alignedBox, Rgba8 const& color );
+void AddVertsForOBB2D( std::vector<Vertex>& verts, OBB2 const& orientedBox, Rgba8 const& color );
+void AddVertsForCapsule2D( std::vector<Vertex>& verts, Vec2 const& boneStart, Vec2 const& boneEnd, float radius, Rgba8 const& color, int numSides );
+void AddVertsForTriangle2D( std::vector<Vertex>& verts, Vec2 const& ccw0, Vec2 const& ccw1, Vec2 const& ccw2, Rgba8 const& color );
+void AddVertsForLineSegment2D( std::vector<Vertex>& verts, Vec2 const& start, Vec2 const& end, float thickness, Rgba8 const& color );
