2d28bfc8434549ae31d6f425a368d847e71e785d
(C35) Ruoxin Sun <sylviasun@smu.edu>
2026-02-20 14:06:06 -0600

    Branching Protogame2D to Protogame3D
    [git-p4: depot-paths = "//depot/C35/Students/sylviasun/SD/": change = 451368]

---
+++ Protogame3D/Code/Game/App.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "Engine/Core/EventSystem.hpp"
+#include "Engine/Renderer/Camera.hpp"
+
+
+//-----------------------------------------------------------------------------------------------
+class App;        // Forward declaration
+class Engine;     // Forward declaration
+class Game;       // Forward declaration
+class PlayerShip; // Forward declaration
+
+
+//-----------------------------------------------------------------------------------------------
+extern App* g_app;
+
+
+//-----------------------------------------------------------------------------------------------
+class App
+{
+public:
+	App();
+	~App();
+
+	void RunMainLoop();
+	void RunFrame();
+	void Update();
+	void UpdateFromKeyboard();
+	void UpdateFromController();
+	void Render()									const;
+
+	static bool Command_Quit( EventArgs& args );
+	void SetIsQuitting();
+	bool IsQuitting()								const;
+	void HardReset();
+
+public:
+	Game*			m_game = nullptr;
+
+	bool			m_isQuitting = false;
+};
