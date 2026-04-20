0ae8274532d7327980880289abf60e9881e3cf82
(C35) Ruoxin Sun <sylviasun@smu.edu>
2026-04-20 15:55:59 -0600

    Implement easing function rendering
    [git-p4: depot-paths = "//depot/C35/Students/sylviasun/SD/": change = 458725]

--- MathVisualTests/Code/Game/Game2DCurves.cpp
+++ MathVisualTests/Code/Game/Game2DCurves.cpp
@@ -2,11 +2,14 @@
 #include "Game/GameCommon.hpp"
 #include "Engine/Core/Engine.hpp"
 #include "Engine/Core/Rgba8.hpp"
-#include "Engine/Core/Vertex.hpp"
+#include "Engine/Core/SimpleTriangleFont.hpp"
 #include "Engine/Core/VertexUtils.hpp"
+#include "Engine/Math/Easing.hpp"
+#include "Engine/Math/MathUtils.hpp"
+#include "Engine/Math/Vec2.hpp"
 #include "Engine/Renderer/Camera.hpp"
 #include "Engine/Renderer/Renderer.hpp"
-#include <vector>
+
 
 
 //-----------------------------------------------------------------------------------------------
@@ -24,6 +27,14 @@ Game2DCurves::Game2DCurves()
 	m_splinePanel.PadOnAllSides( panelPadding );
 	m_bezierPanel.PadOnAllSides( panelPadding );
 	m_easingPanel.PadOnAllSides( panelPadding );
+
+	float easingFunctionGraphSize = m_easingPanel.GetDimensions().y * 0.85f;
+	float halfGraphSize = easingFunctionGraphSize * 0.5f;
+	float graphCenterX = m_easingPanel.GetCenter().x;
+
+	m_easingFunctionGraphBounds = AABB2(
+		Vec2( graphCenterX - halfGraphSize, m_easingPanel.m_maxs.y - easingFunctionGraphSize ),
+		Vec2( graphCenterX + halfGraphSize, m_easingPanel.m_maxs.y ) );
 }
 
 
@@ -47,6 +58,23 @@ void Game2DCurves::UpdateFromKeyboard( [[maybe_unused]] float deltaSeconds )
 	{
 		m_isDebugDraw = !m_isDebugDraw;
 	}
+
+	if ( g_engine->m_inputSystem->WasKeyJustPressed( 'W' ) )
+	{
+		m_easingFunction = ( EasingFunctionLabel ) ( ( int ) m_easingFunction - 1 );
+		if ( m_easingFunction < EasingFunctionLabel::IDENTITY )
+		{
+			m_easingFunction = ( EasingFunctionLabel ) ( ( int ) EasingFunctionLabel::NUM_EASING_FUNCTIONS - 1 );
+		}
+	}
+	if ( g_engine->m_inputSystem->WasKeyJustPressed( 'E' ) )
+	{
+		m_easingFunction = ( EasingFunctionLabel ) ( ( int ) m_easingFunction + 1 );
+		if ( m_easingFunction >= EasingFunctionLabel::NUM_EASING_FUNCTIONS )
+		{
+			m_easingFunction = EasingFunctionLabel::IDENTITY;
+		}
+	}
 }
 
 
@@ -69,8 +97,114 @@ void Game2DCurves::Render() const
 		AddVertsForAABB2D( verts, m_easingPanel, panelColor, Vec2::ZERO, Vec2::ONE );
 	}
 
+	// Easing functions
+	Rgba8 const graphBackgroundColor( 0, 0, 100 );
+	Rgba8 const graphLineColor( 0, 255, 0 );
+	AddVertsForAABB2D( verts, m_easingFunctionGraphBounds, graphBackgroundColor, Vec2::ZERO, Vec2::ONE );
+
+	std::string easingFunctionLabel = GetEasingFunctionLabel();
+	RenderEasingFunctionGraph( verts );
+	
+	if ( !easingFunctionLabel.empty() )
+	{
+		float const labelCellHeight = 3.5f;
+		float const labelWidth = GetSimpleTriangleStringWidth( easingFunctionLabel, labelCellHeight );
+		Vec2 const labelStartMins(
+			m_easingFunctionGraphBounds.GetCenter().x - ( labelWidth * 0.5f ),
+			m_easingFunctionGraphBounds.m_mins.y - labelCellHeight - 1.5f );
+
+		AddVertsForTextTriangles2D( verts, easingFunctionLabel, labelStartMins, labelCellHeight, graphLineColor );
+	}
+
 	g_engine->m_renderer->BindTexture( nullptr );
 	g_engine->m_renderer->DrawVertexArray( verts );
 
 	g_engine->m_renderer->EndCamera( *m_worldCamera );
+}
+
+
+//-----------------------------------------------------------------------------------------------
+std::string Game2DCurves::GetEasingFunctionLabel() const
+{
+	switch ( m_easingFunction )
+	{
+		case EasingFunctionLabel::IDENTITY:			return "Identity";
+		case EasingFunctionLabel::SMOOTH_START_2:	return "SmoothStart2";
+		case EasingFunctionLabel::SMOOTH_START_3:	return "SmoothStart3";
+		case EasingFunctionLabel::SMOOTH_START_4:	return "SmoothStart4";
+		case EasingFunctionLabel::SMOOTH_START_5:	return "SmoothStart5";
+		case EasingFunctionLabel::SMOOTH_START_6:	return "SmoothStart6";
+		case EasingFunctionLabel::SMOOTH_STOP_2:	return "SmoothStop2";
+		case EasingFunctionLabel::SMOOTH_STOP_3:	return "SmoothStop3";
+		case EasingFunctionLabel::SMOOTH_STOP_4:	return "SmoothStop4";
+		case EasingFunctionLabel::SMOOTH_STOP_5:	return "SmoothStop5";
+		case EasingFunctionLabel::SMOOTH_STOP_6:	return "SmoothStop6";
+		case EasingFunctionLabel::SMOOTH_STEP_3:	return "SmoothStep3";
+		case EasingFunctionLabel::SMOOTH_STEP_5:	return "SmoothStep5";
+		case EasingFunctionLabel::HESITATE_3:		return "Hesitate3";
+		case EasingFunctionLabel::HESITATE_5:		return "Hesitate5";
+		case EasingFunctionLabel::CUSTOM:			return "CustomFunkyEasingFunction";
+		default: 									return "";
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+EasingFunction Game2DCurves::GetEasingFunction() const
+{
+	switch ( m_easingFunction )
+	{
+		case EasingFunctionLabel::IDENTITY:			return Identity;
+		case EasingFunctionLabel::SMOOTH_START_2:	return SmoothStart2;
+		case EasingFunctionLabel::SMOOTH_START_3:	return SmoothStart3;
+		case EasingFunctionLabel::SMOOTH_START_4:	return SmoothStart4;
+		case EasingFunctionLabel::SMOOTH_START_5:	return SmoothStart5;
+		case EasingFunctionLabel::SMOOTH_START_6:	return SmoothStart6;
+		case EasingFunctionLabel::SMOOTH_STOP_2:	return SmoothStop2;
+		case EasingFunctionLabel::SMOOTH_STOP_3:	return SmoothStop3;
+		case EasingFunctionLabel::SMOOTH_STOP_4:	return SmoothStop4;
+		case EasingFunctionLabel::SMOOTH_STOP_5:	return SmoothStop5;
+		case EasingFunctionLabel::SMOOTH_STOP_6:	return SmoothStop6;
+		case EasingFunctionLabel::SMOOTH_STEP_3:	return SmoothStep3;
+		case EasingFunctionLabel::SMOOTH_STEP_5:	return SmoothStep5;
+		case EasingFunctionLabel::HESITATE_3:		return Hesitate3;
+		case EasingFunctionLabel::HESITATE_5:		return Hesitate5;
+		case EasingFunctionLabel::CUSTOM:			return CustomFunkyEasingFunction;
+		default: 									return nullptr;
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+void Game2DCurves::RenderEasingFunctionGraph( std::vector<Vertex>& verts ) const
+{
+	Rgba8 const curveColor( 0, 255, 0 );
+	float const lineThickness = 0.2f;
+
+	for ( int segmentIndex = 0; segmentIndex < m_numCurveSegments; ++ segmentIndex )
+	{
+		float t0 = static_cast<float>( segmentIndex ) / static_cast<float>( m_numCurveSegments );
+		float t1 = static_cast<float>( segmentIndex + 1 ) / static_cast<float>( m_numCurveSegments );
+
+		EasingFunction easingFunction = GetEasingFunction();
+		float y0 = easingFunction( t0 );
+		float y1 = easingFunction( t1 );
+
+		Vec2 const p0(
+			RangeMap( t0, 0.f, 1.f, m_easingFunctionGraphBounds.m_mins.x, m_easingFunctionGraphBounds.m_maxs.x ),
+			RangeMap( y0, 0.f, 1.f, m_easingFunctionGraphBounds.m_mins.y, m_easingFunctionGraphBounds.m_maxs.y ) );
+
+		Vec2 const p1(
+			RangeMap( t1, 0.f, 1.f, m_easingFunctionGraphBounds.m_mins.x, m_easingFunctionGraphBounds.m_maxs.x ),
+			RangeMap( y1, 0.f, 1.f, m_easingFunctionGraphBounds.m_mins.y, m_easingFunctionGraphBounds.m_maxs.y ) );
+
+		AddVertsForLineSegment2D( verts, p0, p1, lineThickness, curveColor );
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------
+float Game2DCurves::CustomFunkyEasingFunction( float t )
+{
+	return t;
 }
--- MathVisualTests/Code/Game/Game2DCurves.hpp
+++ MathVisualTests/Code/Game/Game2DCurves.hpp
@@ -1,6 +1,36 @@
 #pragma once
 #include "Game/Game.hpp"
 #include "Engine/Math/AABB2.hpp"
+#include "Engine/Core/Vertex.hpp"
+#include <string>
+#include <vector>
+
+
+//-----------------------------------------------------------------------------------------------
+typedef float ( *EasingFunction )( float t );
+
+
+//-----------------------------------------------------------------------------------------------
+enum class EasingFunctionLabel
+{
+	IDENTITY,
+	SMOOTH_START_2,
+	SMOOTH_START_3,
+	SMOOTH_START_4,
+	SMOOTH_START_5,
+	SMOOTH_START_6,
+	SMOOTH_STOP_2,
+	SMOOTH_STOP_3,
+	SMOOTH_STOP_4,
+	SMOOTH_STOP_5,
+	SMOOTH_STOP_6,
+	SMOOTH_STEP_3,
+	SMOOTH_STEP_5,
+	HESITATE_3,
+	HESITATE_5,
+	CUSTOM,
+	NUM_EASING_FUNCTIONS
+};
 
 
 //-----------------------------------------------------------------------------------------------
@@ -15,8 +45,20 @@ public:
 	void Render() const override;
 
 private:
+	std::string GetEasingFunctionLabel() const;
+	EasingFunction GetEasingFunction() const;
+	void RenderEasingFunctionGraph( std::vector<Vertex>& verts ) const;
+	static float CustomFunkyEasingFunction( float t );
+
+private:
+	int m_numCurveSegments = 64;
 	bool m_isDebugDraw = false;
+
 	AABB2 m_splinePanel = AABB2::ZERO_TO_ONE;
 	AABB2 m_bezierPanel = AABB2::ZERO_TO_ONE;
 	AABB2 m_easingPanel = AABB2::ZERO_TO_ONE;
+
+	// Easing function
+	AABB2 m_easingFunctionGraphBounds = AABB2::ZERO_TO_ONE;
+	EasingFunctionLabel m_easingFunction = EasingFunctionLabel::IDENTITY;
 };
